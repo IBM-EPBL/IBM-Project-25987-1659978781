2bba48823f5ba4e86f69fafd58a81711348f7fcf
Anu_gt <63920485+Anu232@users.noreply.github.com>
2022-11-17 12:29:35 +0530

    Update App.py

--- Assignments/Anushree/Assignment 3/cloud object storage/App.py
+++ Assignments/Anushree/Assignment 3/cloud object storage/App.py
@@ -3,8 +3,8 @@ import ibm_boto3
 from ibm_botocore.client import Config, ClientError
 
 COS_ENDPOINT="https://s3.jp-tok.cloud-object-storage.appdomain.cloud"
-COS_API_KEY_ID="v7JvGIH7V0f3UZL6FvgPU0zs-rgpZKAvj2W3DoClvRRA"
-COS_INSTANCE_CRN="crn:v1:bluemix:public:cloud-object-storage:global:a/3d4040a37c90488084c76adf29cf1ad7:d1356827-f772-4c07-b31e-68e17dfcb914::"
+COS_API_KEY_ID="p-JwPZ4KM1XIXHLzxaVvIfZK7MMT1vBMfaMmu3NeflYK"
+COS_INSTANCE_CRN="crn:v1:bluemix:public:iam-identity::a/e15007b010194c50af36903539c12f1c::serviceid:ServiceId-d8bad00d-85df-483c-a06f-2b90721ddf30"
 
 
 
